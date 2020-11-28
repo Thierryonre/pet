0e5870b71248e5b427e5c5b89b3c626f7e94d12e
Thierryonre <Thierry_popat@hotmail.com>
2020-11-28 06:08:02 +0000

    Update pvp.py

--- pet/pvp.py
+++ pet/pvp.py
@@ -23,13 +23,39 @@ import torch
 from transformers import PreTrainedTokenizer, GPT2Tokenizer
 from pet.utils import InputExample, get_verbalization_ids
 
-import log
+# import log
 from pet import wrapper as wrp
 
-logger = log.get_logger('root')
+import logging
+
+names = set()
+
+def __setup_custom_logger(name: str) -> logging.Logger:
+    root_logger = logging.getLogger()
+    root_logger.handlers.clear()
+
+    formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
+
+    names.add(name)
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(handler)
+    return logger
+
+
+def get_logger(name: str) -> logging.Logger:
+    if name in names:
+        return logging.getLogger(name)
+    else:
+        return __setup_custom_logger(name)
+
+logger = get_logger('root')
 
 FilledPattern = Tuple[List[Union[str, Tuple[str, bool]]], List[Union[str, Tuple[str, bool]]]]
-
 
 class PVP(ABC):
     """
