7ed74cf2922c679d2e02aaf2337312fa6def9ea5
Thierryonre <Thierry_popat@hotmail.com>
2020-11-28 06:10:09 +0000

    Update tasks.py

--- pet/tasks.py
+++ pet/tasks.py
@@ -22,9 +22,37 @@ from abc import ABC, abstractmethod
 from collections import defaultdict, Counter
 from typing import List, Dict, Callable
 
-import log
+# import log
 from pet import task_helpers
 from pet.utils import InputExample
+
+import logging
+
+names = set()
+
+def __setup_custom_logger(name: str) -> logging.Logger:
+    root_logger = logging.getLogger()
+    root_logger.handlers.clear()
+
+    formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
+
+    names.add(name)
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(handler)
+    return logger
+
+
+def get_logger(name: str) -> logging.Logger:
+    if name in names:
+        return logging.getLogger(name)
+    else:
+        return __setup_custom_logger(name)
+
 
 logger = log.get_logger('root')
 
