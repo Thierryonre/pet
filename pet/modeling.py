a23cb66ef8adba59db04feaead4a0e65e5dc7788
Thierryonre <Thierry_popat@hotmail.com>
2020-11-28 06:14:39 +0000

    Update modeling.py

--- pet/modeling.py
+++ pet/modeling.py
@@ -24,11 +24,38 @@ import torch
 from sklearn.metrics import f1_score
 from transformers.data.metrics import simple_accuracy
 
-import log
+# import log
 from pet.utils import InputExample, exact_match, save_logits, save_predictions, softmax, LogitsList, set_seed, eq_div
 from pet.wrapper import TransformerModelWrapper, SEQUENCE_CLASSIFIER_WRAPPER, WrapperConfig
 
-logger = log.get_logger('root')
+import logging
+
+names = set()
+
+def __setup_custom_logger(name: str) -> logging.Logger:
+    root_logger = logging.getLogger()
+    root_logger.handlers.clear()
+
+    formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
+
+    names.add(name)
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(handler)
+    return logger
+
+
+def get_logger(name: str) -> logging.Logger:
+    if name in names:
+        return logging.getLogger(name)
+    else:
+        return __setup_custom_logger(name)
+
+logger = get_logger('root')
 
 
 class PetConfig(ABC):
